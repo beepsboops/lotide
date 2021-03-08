c099e15eb58b7f43f1a11438fb2f8cd5c0e4fa3c
A <awongpro7@gmail.com>
2021-03-08 22:20:56 +0000

    Removed some unnecessary emoji code

--- head.js
+++ head.js
@@ -2,9 +2,9 @@
 
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(String.fromCodePoint(0x1F44D, 0x1F44D, 0x1F44D) + ` Assertion Passed: ${actual} === ${expected}`);
+    console.log(String.fromCodePoint(0x1F44D) + ` Assertion Passed: ${actual} === ${expected}`);
   } else if (actual !== expected) {
-    console.log(String.fromCodePoint(0x1F4A9, 0x1F4A9, 0x1F4A9) + ` Assertion Failed: ${actual} !== ${expected}`);
+    console.log(String.fromCodePoint(0x1F4A9) + ` Assertion Failed: ${actual} !== ${expected}`);
   } return assertEqual;
 };
 const head = function(array) {
