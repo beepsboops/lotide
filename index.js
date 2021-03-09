850c96c899d4caef389786853ccbd7c26b29de83
A <awongpro7@gmail.com>
2021-03-09 03:38:23 +0000

    Adding index.js file for Exporting All Functions II assingment

---
+++ index.js
@@ -0,0 +1,28 @@
+// index.js
+assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters  = require('./countLetters');
+const countOnly = required('./countOnly');
+const eqArrays = required('./eqArrays');
+const head   = require('./head');
+const letterPositions = ('./letterPositions');
+const map = require('./map');
+const middle = require('./middle');
+const tail   = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual:  assertEqual,
+  countLetters: countLetters,
+  countOnly:  countOnly,
+  eqArrays: eqArrays,
+  head:   head,
+  letterPositions:  letterPositions,
+  map:  map,
+  middle: middle,
+  tail:   tail,
+  takeUntil:  takeUntil,
+  without:  without,
+};
