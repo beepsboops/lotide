98209afcce7a47d5c7134902aab9bbdc81d403e7
A <awongpro7@gmail.com>
2021-03-09 01:18:43 +0000

    Adding modified file for Incorporate Mocha and Chai assignment

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,16 @@
 // test/headTest.js
 const assertEqual = require('../assertEqual');
+// const head = require('../head.js');
 
-const head = require('../head.js');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-//TEST CODE FROM head.js
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+});
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+it("returns '5' for ['5']", () => {
+  assert.strictEqual(head(['5']), '5'); 
+});
