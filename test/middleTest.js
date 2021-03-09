340eb3a2e7ba910367b21c86a40c45f5977e9714
A <awongpro7@gmail.com>
2021-03-09 03:25:37 +0000

    Adding files for Refactor with Mocha and Chai I

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,17 @@
 // test/middleTest.js
 const middle = require('../middle')
 const assertArraysEqual = require('../assertArraysEqual')
+const assert = require('chai').assert
 
 // TEST CODE FROM middle.js
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]))
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]))
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14]))
+// console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15]))
+
+describe("#middle", () => {
+  it("returns [2,3] for [1, 2, 3, 4", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]),[2,3]);
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]),[2]);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,15 @@
 // test/tailTest.js
-const assertEqual = require('../assertEqual')
-const tail = require('../tail')
+const assertEqual = require('chai').assert; //changed assertEqual -> assert.deepEqual; not sure if this is what instructions meant
+const tail = require('../tail');
+const assert = require('chai').assert;
 
 
 //TEST CODE FROM tail.js
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(assertEqual(tail(words), ["Lighthouse", "Labs"])); // Do i want my test case to look like this?
+console.log(assertEqual(tail(words), ["Lighthouse", "Labs"])); // Do i want my test case to look like this?; deep equal for arrays or objects; remove this 
+
+describe("#tail", () => {
+  it("describes what I'm testing - am I getting the tail", () => {
+    assert.deepEqual(tail(words), ["Lighthouse", "Labs"]);
+  });
+});
